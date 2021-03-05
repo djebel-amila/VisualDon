6f8f647c67ca696b33d31a27292a3b72c7bf53ff
djebel-amila <jmerc001@gold.ac.uk>
2021-03-05 16:11:05 +0100

    update

---
+++ 20210305/exercice_3.js
@@ -0,0 +1,26 @@
+const data = require('./biosentiers-all-events.json')
+
+// const listSpeciesId = d => d.properties.speciesId && d.properties.speciesId !== "obersono-bers-onob-erso-nobersonober" est équivalent à : 
+function listSpeciesId(d) {
+    return d.properties.speciesId && d.properties.speciesId !== "obersono-bers-onob-erso-nobersonober"
+}
+
+
+const reducer = (r , d) => {
+    if (r[d.properties.speciesId]) { 
+      return { ...r, [d.properties.speciesId]: r[d.properties.speciesId] + 1 }
+   } else {
+        return {...r, [d.properties.speciesId]: 1}
+   }
+ }
+
+const SpeciesIdCount = data.filter(listSpeciesId).reduce(reducer,{})
+
+// console.log(typeof(SpeciesIdCount))
+
+const data2 = require('./data-package.json')
+const data3 = data2.species
+    .map(d => ({ speciesId: d.id, name: d.commonName.fr, count: SpeciesIdCount[d.id] }))
+    .filter(d => d.count)
+
+console.log(JSON.stringify(data3))
