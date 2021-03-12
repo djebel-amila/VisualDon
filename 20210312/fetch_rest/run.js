e706cfda5ab362b725ae06f11181140b0de0f86f
djebel-amila <jmerc001@gold.ac.uk>
2021-03-12 13:36:47 +0100

    update

---
+++ 20210312/fetch_rest/run.js
@@ -0,0 +1,31 @@
+const R = require('ramda')
+const fetch = require('node-fetch')
+
+const URL_USERS = 'https://jsonplaceholder.typicode.com/users'
+const URL_POSTS = 'https://jsonplaceholder.typicode.com/posts'
+
+
+const fetchUsers = async () => {
+
+const users = fetch(URL_USERS)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+const posts = posts = fetch(URL_POSTS)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+}
+
+fetchUsers()
+
+/* 
+filter 
+usernames
+ville
+company.name 
+titres_posts
+
+*/
+
+
