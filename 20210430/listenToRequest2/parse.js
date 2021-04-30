509919cf80486977daf7071730a5bf5809852922
djebel-amila <jmerc001@gold.ac.uk>
2021-04-30 12:18:52 +0200

    update

---
+++ 20210430/listenToRequest2/parse.js
@@ -0,0 +1,20 @@
+const R = require('ramda')
+const testdata = require('./testdata.json')
+
+module.exports = data => {
+  const products = data[0].data.search.products
+
+  const getProductData = d => {
+    const product = R.pipe(
+      R.prop('product'),
+      R.pick(['productId', 'name', 'nameProperties', 'brandId', 'brandName', 'averageRating'])
+    )(d)
+    const price = R.path(['offer', 'price', 'amountIncl'], d)
+    return { ...product, price }
+  }
+
+  return {
+    data: products.results.map(getProductData),
+    ...R.pick(['nextOffset', 'total'], products)
+  }
+}
---
+++ 20210430/listenToRequest2/run.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const get = require('./get')
+//const data = require('./testdata.json')
+
+const parse = require('./parse')
+
+const file = fs.createWriteStream('bike.ndjson')
+
+const loop = offset => {
+  get(offset)
+    .then(res => {
+      const { nextOffset, total, data } = parse(res)
+      data.forEach(d => file.write(`${JSON.stringify(d)}\n`))
+      if (nextOffset < total) {
+        loop(nextOffset)
+      }
+    })
+}
+
+loop(0)
